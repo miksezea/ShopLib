c991df6f72644ac4acaa28fe9717ce98542f7326
miksezea <mikk289e@edu.zealand.dk>
2023-03-28 09:30:56 +0200

    Add project files.

---
+++ ShopLib/Models/ShoppingList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopLib.Models
+{
+    public class ShoppingList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {this.Id}. Name: {this.Name}. Price: {this.Price}. Quantity: {this.Quantity}.";
+        }
+    }
+}
